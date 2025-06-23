0fc7a63ed0764612a438c17e21f0b43b47101c20
fepfitra <fitriansyahekaputra2001@mail.ugm.ac.id>
2025-06-23 10:12:43 +0700

    feat: remove rosenbrock add initpoints

--- src/lib.rs
+++ src/lib.rs
@@ -1,35 +1,151 @@
 use std::vec;
+// use std::default::Default;
 
-struct MGHOptimal {}
-impl MGHOptimal {
-    fn rosenbrock() -> Vec<f64> {
-        vec![1.0, 1.0]
+struct MGHInit {}
+
+impl MGHInit {
+    fn hellical_valley() -> Vec<f64> {
+        vec![-1., 0., 0.]
+    }
+
+    fn biggs_EXP6() -> Vec<f64> {
+        vec![1., 2., 1., 1., 1., 1.]
+    }
+
+    fn gaussian() -> Vec<f64> {
+        vec![0.4, 1., 0.]
+    }
+
+    fn powell_badly_scaled() -> Vec<f64> {
+        vec![0., 1.]
+    }
+
+    fn box_3d() -> Vec<f64> {
+        vec![0., 10., 20.]
+    }
+
+    fn variably_dimensional(n: usize) -> Vec<f64> {
+        let mut vec = vec![0.; n];
+        for i in 0..n {
+            vec[i] = 1. - (i as f64 + 1.) / (n as f64);
+        }
+        vec
+    }
+
+    fn watson(n: usize) -> Vec<f64> {
+        vec![0.; n]
+    }
+
+    fn penalty1(n: usize) -> Vec<f64> {
+        let mut vec = vec![0.; n];
+        for i in 0..n {
+            vec[i] = i as f64 + 1.;
+        }
+        vec
+    }
+
+    fn penalty2(n: usize) -> Vec<f64> {
+        vec![0.5; n]
+    }
+
+    fn brown_badly_scaled() -> Vec<f64> {
+        vec![1., 1.]
+    }
+
+    fn brown_and_dennis() -> Vec<f64> {
+        vec![25., 5., -0.5, -1.]
+    }
+
+    fn gulf_research_and_development() -> Vec<f64> {
+        vec![5., 2.5, 0.15]
+    }
+
+    fn trigonometric(n: usize) -> Vec<f64> {
+        vec![1. / (n as f64); n]
+    }
+
+    fn extended_rosenbrock(n: usize) -> Vec<f64> {
+        let mut vec = vec![0.; n];
+        for i in 0..n {
+            vec[i] = if (i & 1) == 1 { 1.0 } else { -1.2 };
+        }
+        vec
+    }
+
+    fn powell_singular() -> Vec<f64> {
+        vec![3., -1., 0., 1.]
+    }
+
+    fn beale() -> Vec<f64> {
+        vec![1., 1.]
+    }
+
+    fn wood() -> Vec<f64> {
+        vec![-3., -1., -3., -1.]
+    }
+
+    fn chebyquad(n: usize) -> Vec<f64> {
+        let mut vec = vec![0.; n];
+        for i in 0..n {
+            vec[i] = (i as f64 + 1.) / (n as f64 + 1.0)
+        }
+        vec
     }
 }
 
 struct MGH {}
 
-impl MGH {
-    fn rosenbrock(x: Vec<f64>) -> f64 {
-        if x.len() != 2 {
-            panic!("Input vector must have exactly two elements.");
-        }
-        let x1 = x[0];
-        let x2 = x[1];
-
-        let f1 = 10.0 * (x2 - x1.powi(2));
-        let f2 = 1.0 - x1;
-        f1.powi(2) + f2.powi(2)
-    }
-}
+impl MGH {}
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn rosenbrock() {
-        let result = MGH::rosenbrock(MGHOptimal::rosenbrock());
-        assert_eq!(result, 0.0);
+    fn test_init() {
+        let hellical_valley = MGHInit::hellical_valley();
+        assert_eq!(hellical_valley, vec![-1., 0., 0.]);
+
+        let biggs_EXP6 = MGHInit::biggs_EXP6();
+        assert_eq!(biggs_EXP6, vec![1., 2., 1., 1., 1., 1.]);
+
+        let gaussian = MGHInit::gaussian();
+        assert_eq!(gaussian, vec![0.4, 1., 0.]);
+
+        let powell_badly_scaled = MGHInit::powell_badly_scaled();
+        assert_eq!(powell_badly_scaled, vec![0., 1.]);
+
+        let box_3d = MGHInit::box_3d();
+        assert_eq!(box_3d, vec![0., 10., 20.]);
+
+        let variably_dim = MGHInit::variably_dimensional(3);
+        assert_eq!(variably_dim, vec![1. - 1. / 3., 1. - 2. / 3., 1. - 3. / 3.]);
+
+        let watson = MGHInit::watson(3);
+        assert_eq!(watson, vec![0., 0., 0.]);
+
+        let penalty1 = MGHInit::penalty1(3);
+        assert_eq!(penalty1, vec![1., 2., 3.]);
+
+        let penalty2 = MGHInit::penalty2(3);
+        assert_eq!(penalty2, vec![0.5, 0.5, 0.5]);
+
+        let brown_badly_scaled = MGHInit::brown_badly_scaled();
+        assert_eq!(brown_badly_scaled, vec![1., 1.]);
+
+        let brown_and_dennis = MGHInit::brown_and_dennis();
+        assert_eq!(brown_and_dennis, vec![25., 5., -0.5, -1.]);
+
+        let gulf_research_and_development = MGHInit::gulf_research_and_development();
+        assert_eq!(gulf_research_and_development, vec![5., 2.5, 0.15]);
+
+        let trigonometric = MGHInit::trigonometric(3);
+        assert_eq!(trigonometric, vec![1. / 3., 1. / 3., 1. / 3.]);
+
+        let extended_rosenbrock = MGHInit::extended_rosenbrock(3);
+        assert_eq!(extended_rosenbrock, vec![-1.2, 1., -1.2]);
+
+        let powell_singular = MGHInit::powell_singular();
+        assert_eq!(powell_singular, vec![3., -1., 0., 1.]);
     }
 }
