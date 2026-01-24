3658b193a42ad02be6e22d189dd310ae93c67508
fepfitra <fitriansyahekaputra2001@mail.ugm.ac.id>
2026-01-24 21:52:55 +0700

    feat(hellical_valley): update formulae and min

--- src/problems/hellical_valley.rs
+++ src/problems/hellical_valley.rs
@@ -15,8 +15,13 @@ pub fn hellical_valley(x: &[f64]) -> f64 {
         (x2 / x1).atan() / (2. * PI) + 0.5
     };
 
+    // f_1(x) = 10[x3 - 10\theta(x1, x2)]
     let f1 = 10. * (x3 - 10. * theta);
+
+    // f_2(x) = 10[(x1^2 + x2^2)^0.5 - 1]
     let f2 = 10. * ((x1.powi(2) + x2.powi(2)).sqrt() - 1.);
+
+    // f_3(x) = x3
     let f3 = x3;
 
     f1.powi(2) + f2.powi(2) + f3.powi(2)
@@ -24,6 +29,10 @@ pub fn hellical_valley(x: &[f64]) -> f64 {
 
 pub fn init() -> Vec<f64> {
     vec![-1., 0., 0.]
+}
+
+pub fn min() -> Vec<f64> {
+    vec![1., 0., 0.]
 }
 
 #[cfg(test)]
@@ -35,5 +44,12 @@ mod tests {
         let x = init();
         let val = hellical_valley(&x);
         assert!(val.is_finite());
+    }
+
+    #[test]
+    fn test_min() {
+        let x = min();
+        let val = hellical_valley(&x);
+        assert!(val.abs() < 1e-8);
     }
 }
