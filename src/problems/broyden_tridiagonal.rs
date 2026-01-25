82be962c6bde2abf3c731b0ebfd33f5c3cd63b1f
fepfitra <fitriansyahekaputra2001@mail.ugm.ac.id>
2026-01-26 00:18:09 +0700

    docs(broyden_tridiagonal): add formula comments

--- src/problems/broyden_tridiagonal.rs
+++ src/problems/broyden_tridiagonal.rs
@@ -5,6 +5,7 @@ pub fn broyden_tridiagonal(x: &[f64]) -> f64 {
 
     let mut res = 0.;
     for i in 1..(new_x.len() - 1) {
+        // f_i(x) = (3 - 2x_i)x_i - x_{i-1} - 2x_{i+1} + 1
         let f = (3. - 2. * new_x[i]) * new_x[i] - new_x[i - 1] - 2. * new_x[i + 1] + 1.;
         res += f.powi(2);
     }
@@ -27,5 +28,14 @@ mod tests {
         assert_eq!(x[0], -1.0);
         let val = broyden_tridiagonal(&x);
         assert!(val.is_finite());
+    }
+
+    #[test]
+    fn test_value_n10() {
+        let n = 10;
+        let x = init(n);
+        let val = broyden_tridiagonal(&x);
+        // At x=(-1,...), f = n + 11 = 21
+        assert!((val - 21.0).abs() < 1e-8);
     }
 }
