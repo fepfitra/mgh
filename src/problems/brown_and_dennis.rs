15e6926bc23bab2a5e42f82d3cb95f375ce12bb6
fepfitra <fitriansyahekaputra2001@mail.ugm.ac.id>
2026-01-25 23:26:48 +0700

    fix(brown_and_dennis): correct formula and init vector

--- src/problems/brown_and_dennis.rs
+++ src/problems/brown_and_dennis.rs
@@ -11,16 +11,18 @@ pub fn brown_and_dennis(x: &[f64], m: usize) -> f64 {
     let mut res = 0.;
 
     for i in 1..(m + 1) {
-        // let index = i - 1;
+        // t_i = i / 5
         let t = (i as f64) / 5.;
+
+        // f_i(x) = (x_1 + t_i x_2 - e^t_i)^2 + (x_3 + x_4 sin(t_i) - cos(t_i))^2
         let f = (x1 + t * x2 - E.powf(t)).powi(2) + (x3 + x4 * t.sin() - t.cos()).powi(2);
-        res += f.powi(2);
+        res += f;
     }
     res
 }
 
 pub fn init() -> Vec<f64> {
-    vec![25., 5., -1., -1.]
+    vec![25., 5., -5., -1.]
 }
 
 #[cfg(test)]
